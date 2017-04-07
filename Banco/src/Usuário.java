a61b4954ad2c74526205f9302e8f4e2bed75cd24
Pedro Santos <Pedro Santos>
2017-04-07 14:49:23 -0300

    Atv

---
+++ Banco/src/Usuário.java
@@ -0,0 +1,19 @@
+
+public class Usuário {
+	//nome da classe
+	
+	//definindo atributos
+	String nome;
+	int cpf;
+	int agencia;
+	int conta;
+	double saldo;
+	int sl = 1250;
+	Usuário() {
+		
+	}
+	void saldo() {
+		
+		sl = sl+sl;
+	}
+}
